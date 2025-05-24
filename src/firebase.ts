abcd64edc4be5d970ff03e6c944bf7d03bdbacab
BismayDey <bismaydey001@gmail.com>
2025-05-24 15:42:17 +0530

    changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAdI2OCc24EcTW-kmSeJLqZx0Gzav1YhoQ",
   authDomain: "linked-4afa2.firebaseapp.com",
   projectId: "linked-4afa2",
   storageBucket: "linked-4afa2.firebasestorage.app",
   messagingSenderId: "962680318028",
-  appId: "1:962680318028:web:51f51ec807d70240626ce9"
+  appId: "1:962680318028:web:51f51ec807d70240626ce9",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
